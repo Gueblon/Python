225f7e164b7f224593c52c9b2bf30e278e563e01
Щериканова Евгения Александровна <EShcherikanova@office.msk.mtt>
2020-02-09 21:34:23 +0300

    Upd

--- Game/game.py
+++ Game/game.py
@@ -6,10 +6,28 @@
 # Создаем игру и окно
 import pygame #импортирование модуля pygame
 pygame.init() #инициализация модуля
-win = pygame.display.set_mode((813,411)) #задание размера экрана
+screen = pygame.display.set_mode((813,411)) #задание размера экрана
 pygame.display.set_caption("Играйся со мной") #заголовок окна
+
+
+#-----------------------------------------
+# Определение объекта спрайта игрока
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+    # Вызвать конструктор родительского класса (Sprite)
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('Sprites/1 Woodcutter/Woodcutter_walk.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = (50, 200)
+
+    def update(self):
+        self.rect.x += 5
+        if self.rect.left > 813:
+            self.rect.right = 0
+
 #-----------------------------------------
 # загрузка спрайтов
+
 walk = pygame.image.load('Sprites/1 Woodcutter/Woodcutter_walk.png')
 jump = pygame.image.load('Sprites/1 Woodcutter/Woodcutter_jump.png')
 bg = pygame.image.load('bg.jpg')
@@ -17,13 +35,15 @@ playerStand = pygame.image.load('Sprites/1 Woodcutter/Woodcutter.png')
 
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
 #-----------------------------------------
-# переменные
-x = 50
-y = 250
-width = 48
-height = 48
+# определяем переменные
+x = 50 # координаты начального положения игрока
+y = 250 # координаты начального положения игрока
+width = 48 # ширина спрайта
+height = 48 # высота спрайта
 speed = 5
 
 isJump = False
@@ -36,43 +56,27 @@ walk = False
 
 def drawWindow ():
     global animCount
-    win.blit(bg, (0, 0)) # заполнение экрана фоном
+    screen.blit(bg, (0, 0)) # заполнение экрана фоном
     if animCount +1 >= 30:
         animCount = 0
 
     if walk:
-        win.blit(walk, (x, y))
+        screen.blit(walk, (x, y)) # отрисовка идущего
         animCount += 1
     else:
-        win.blit(playerStand, (x, y)) # отрисовка стоящего
+        screen.blit(playerStand, (x, y)) # отрисовка стоящего
 
     pygame.display.update()
 
 
 #-----------------------------------------
-# Определение объекта спрайта игрока
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-    # Вызвать конструктор родительского класса (Sprite)
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('Sprites/1 Woodcutter/Woodcutter_walk.png')
-        self.rect = self.image.get_rect()
-        self.rect.center = (48 / 2, 48 / 2)
-
-
-all_sprites = pygame.sprite.Group()
-player = Player()
-all_sprites.add(player)
-
-
-#-----------------------------------------
 # в игре должен быть один основной цикл, чтобы игра не закрывалось
-run = True
-while run:
+running = True
+while running:
     pygame.time.delay(50) # количество милисекунд через которые повторяется данный цикл
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            running = False
     keys = pygame.key.get_pressed() # нажатие клавиш
     if keys[pygame.K_LEFT] and x > 0 - width: # задание размера поля в котором движется объект
         x -= speed
@@ -104,7 +108,5 @@ while run:
             jumpCount = 10
     drawWindow ()
     all_sprites.update()
-#    all_sprites.draw()
-    # После отрисовки всего, переворачиваем экран
-    pygame.display.flip()
+    all_sprites.draw(screen)
 pygame.quit()
